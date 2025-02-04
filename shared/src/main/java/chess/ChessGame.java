ee74738bba50971a428a6f5102769d83d264829c
bigE4 <ianjellsworth@gmail.com>
2025-02-04 12:26:31 -0700

    tests copy to passoff folder, validMovesRaw function declaration (may be redundant, but I want to start here) and object overrides for ChessGame

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -1,6 +1,7 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Objects;
 
 /**
  * For a class that can manage a chess game, making moves on a board
@@ -12,6 +13,8 @@ public class ChessGame {
 
     TeamColor teamTurn;
     ChessBoard chessBoard;
+    ChessPiece whiteKing;
+    ChessPiece blackKing;
 
     public ChessGame() {
         teamTurn = TeamColor.WHITE;
@@ -56,6 +59,18 @@ public class ChessGame {
     }
 
     /**
+     * Gets a valid moves for a piece at the given location
+     *
+     * @param startPosition the piece to get valid moves for
+     * @return Set of valid moves for requested piece, or null if no piece at
+     * startPosition
+     */
+    public Collection<ChessMove> validMovesRaw(ChessPosition startPosition) {
+        ChessPiece chessPiece = chessBoard.getPiece(startPosition);
+        return chessPiece.pieceMoves(chessBoard, startPosition);
+    }
+
+    /**
      * Makes a move in a chess game
      *
      * @param move chess move to preform
@@ -72,6 +87,12 @@ public class ChessGame {
      * @return True if the specified team is in check
      */
     public boolean isInCheck(TeamColor teamColor) {
+        // isInCheck is going to:
+        // go piece by piece, calling validMoves on that piece
+        // adds those validMoves to a superList
+        // iterates through that list and extracts the end position into another list
+        // if any end position in that list is the same as the position of the current team's king
+        // then this function will return true.
         throw new RuntimeException("Not implemented");
     }
 
@@ -112,5 +133,19 @@ public class ChessGame {
      */
     public ChessBoard getBoard() {
         return chessBoard;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessGame chessGame = (ChessGame) o;
+        return teamTurn == chessGame.teamTurn && Objects.equals(chessBoard, chessGame.chessBoard) && Objects.equals(whiteKing, chessGame.whiteKing) && Objects.equals(blackKing, chessGame.blackKing);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(teamTurn, chessBoard, whiteKing, blackKing);
     }
 }
