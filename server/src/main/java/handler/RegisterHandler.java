56ff1fabdcf75f3c18bd68e8b234493fd09b2661
bigE4 <ianjellsworth@gmail.com>
2025-03-02 14:05:49 -0700

    Register Handler work, Register Request modification (Dont need setters for the request object) and RegisterResponse init

--- server/src/main/java/handler/RegisterHandler.java
+++ server/src/main/java/handler/RegisterHandler.java
@@ -1,12 +1,20 @@
 package handler;
 
+import com.google.gson.Gson;
+import service.RegisterService;
+import request.RegisterRequest;
+import response.RegisterResponse;
 import spark.Request;
 import spark.Response;
 import spark.Route;
 
-public class RegisterHandler implements Handler, Route {
+public class RegisterHandler implements Route {
+
+    private final Gson gson = new Gson();
+
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        RegisterRequest registerRequest = gson.fromJson(request.body(), RegisterRequest.class);
         return null;
     }
 }
