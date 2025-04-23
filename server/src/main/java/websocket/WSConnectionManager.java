b2f42b41bec80067a498558ee4866979e971e74f
bigE4 <ianjellsworth@gmail.com>
2025-04-23 17:49:05 -0600

    WSConnectionManager work

--- server/src/main/java/websocket/WSConnectionManager.java
+++ server/src/main/java/websocket/WSConnectionManager.java
@@ -1,4 +1,29 @@
 package websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.util.concurrent.ConcurrentHashMap;
+
 public class WSConnectionManager {
+    public final ConcurrentHashMap<String, WSConnection> connections = new ConcurrentHashMap<>();
+
+    public void add(int gameID, String username, Session session) {
+        WSConnection connection = new WSConnection(gameID, username, session);
+        connections.put(username, connection);
+    }
+
+    public void remove(String username) {
+        connections.remove(username);
+    }
+
+    public void broadcast(ServerMessage message, int gameID) {
+        for (WSConnection connection : connections.values()) {
+            if (connection.isOpen() && connection.getGameID() == gameID) {
+                connection.send(message);
+            } else if (!connection.isOpen()) {
+                connections.remove(connection.getUsername());
+            }
+        }
+    }
 }
--- server/src/main/java/websocket/WSHandler.java
+++ server/src/main/java/websocket/WSHandler.java
@@ -1,4 +1,14 @@
 package websocket;
 
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+
+@WebSocket
 public class WSHandler {
+    private final WSConnectionManager connections = new WSConnectionManager();
+
+    @OnWebSocketMessage
+    public void onMessage() {
+
+    }
 }
