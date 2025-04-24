fd4261d23e4841af69e9d61a3e358e57e9fe6eb4
bigE4 <ianjellsworth@gmail.com>
2025-04-23 19:14:49 -0600

    Serializer made static, WSHandler blocked out...

--- server/src/main/java/websocket/WSHandler.java
+++ server/src/main/java/websocket/WSHandler.java
@@ -1,14 +1,48 @@
 package websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
 import org.eclipse.jetty.websocket.api.annotations.WebSocket;
+import websocket.commands.UserGameCommand;
 
 @WebSocket
 public class WSHandler {
-    private final WSConnectionManager connections = new WSConnectionManager();
+    private WSConnectionManager connectionManager = new WSConnectionManager();
+
+    public WSHandler(WSConnectionManager connectionManager) {
+        this.connectionManager = connectionManager;
+    }
 
     @OnWebSocketMessage
-    public void onMessage() {
+    public void onMessage(Session session, String message) {
+        try {
+            UserGameCommand command = Serializer.CommandDeserializer(message);
 
+            switch (command.getCommandType()) {
+                case CONNECT -> connect(connectionManager);
+                case MAKE_MOVE -> move(connectionManager);
+                case RESIGN -> resign(connectionManager);
+                case LEAVE -> leave(connectionManager);
+            }
+
+        } catch (Exception e) {
+            System.out.println("Error. WSHandler broke something.");
+        }
+    }
+
+    private void connect(WSConnectionManager connectionManager) {
+
+    }
+
+    private void move(WSConnectionManager connectionManager) {
+
+    }
+
+    private void resign(WSConnectionManager connectionManager) {
+
+    }
+
+    private void leave(WSConnectionManager connectionManager) {
+        
     }
 }
--- shared/src/main/java/websocket/Serializer.java
+++ shared/src/main/java/websocket/Serializer.java
@@ -5,21 +5,21 @@ import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
 public class Serializer {
-    Gson gson = new Gson();
+    static Gson gson = new Gson();
 
-    public String CommandSerializer(UserGameCommand command) {
+    public static String CommandSerializer(UserGameCommand command) {
         return gson.toJson(command);
     }
 
-    public UserGameCommand CommandDeserializer(String command) {
+    public static UserGameCommand CommandDeserializer(String command) {
         return gson.fromJson(command, UserGameCommand.class);
     }
 
-    public String MessageSerializer(ServerMessage message) {
+    public static String MessageSerializer(ServerMessage message) {
         return gson.toJson(message);
     }
 
-    public ServerMessage MessageDeserializer(String message) {
+    public static ServerMessage MessageDeserializer(String message) {
         return gson.fromJson(message, ServerMessage.class);
     }
 }
